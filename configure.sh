f18489f3b0132b626785e65f92532a24135d084a
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-01-04 19:46:25 -0600

    generate caffe.pb.h and caffe.pb.cxx from SuperaCore/caffe.proto --kazu

--- configure.sh
+++ configure.sh
@@ -4,10 +4,10 @@
 if [[ -z $FORCE_SUPERA_BASEDIR ]]; then
     # If SUPERA_BASEDIR not set, try to guess
     # Find the location of this script:
-    me="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    SUPERA_BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     # Find the directory one above.
-    export SUPERA_BASEDIR="$( cd "$( dirname "$me" )" && pwd )"
-    unset me;
+    #export SUPERA_BASEDIR="$( cd "$( dirname "$me" )" && pwd )"
+    #unset me;
 else
     export SUPERA_BASEDIR=$FORCE_SUPERA_BASEDIR
 fi
@@ -34,24 +34,7 @@ else
     fi
 fi
 
-if [[ -z $ROOTSYS ]]; then
-    echo
-    echo "****************** PyROOT WARNING ********************"
-    echo "*                                                    *"
-    echo "* Did not find your \$ROOTSYS. To use PyROOT feature, *"
-    echo "* Make sure ROOT.py is installed (comes with ROOT).  *"
-    echo "* You need to export \$PYTHONPATH to include the dir  *"
-    echo "* where ROOT.py exists.                              *"
-    echo "*                                                    *"
-    echo "* Help to install PyROOT? See manual/contact author! *"
-    echo "*                                                    *"
-    echo "******************************************************"
-    echo
-fi
-
-#
 # Check LMDB
-#
 error=0
 if [[ -z $LMDB_INCDIR ]]; then
     printf "\033[91merror\033[00m ... \$LMDB_INCDIR must be set for lmdb headers.\n";
@@ -62,6 +45,7 @@ if [[ -z $LMDB_LIBDIR ]]; then
     error=1;
 fi
 
+# Check protobuf
 if [[ -z $PROTOBUF_INCDIR ]]; then
     printf "\033[91merror\033[00m ... \$PROTOBUF_INCDIR must be set for protobuf headers.\n";
     error=1;
@@ -70,6 +54,11 @@ if [[ -z $PROTOBUF_LIBDIR ]]; then
     printf "\033[91merror\033[00m ... \$PROTOBUF_LIBDIR must be set for protobuf libraries.\n";
     error=1;
 fi
+if [[ -z `command -v protoc` ]]; then
+    printf "\033[91merror\033[00m ... protoc (protobuf compiler) is not available. (needed to generate protobuf header.\n";
+    error=1;
+fi
+
 
 if [ $error -eq 1 ]; then
     case `uname -n` in
@@ -98,9 +87,15 @@ printf "\033[95mSUPERA_LIBDIR\033[00m   = $SUPERA_LIBDIR\n"
 
 mkdir -p $SUPERA_BUILDDIR;
 
+if [ ! -f $SUPERA_BASEDIR/SuperaCore/caffe.pb.h ]; then
+    printf "\033[93mnotice\033[00m ... generating caffe proto-buf source code (one-time operation)\n"
+    protoc $SUPERA_BASEDIR/SuperaCore/caffe.proto --proto_path=$SUPERA_BASEDIR/SuperaCore --cpp_out=$SUPERA_BASEDIR/SuperaCore/
+    mv $SUPERA_BASEDIR/SuperaCore/caffe.pb.cc $SUPERA_BASEDIR/SuperaCore/caffe.pb.cxx
+fi
+
 export LD_LIBRARY_PATH=$SUPERA_LIBDIR:$LD_LIBRARY_PATH
 
-if [ $LARLITE_OS -e 'Darwin' ]; then
+if [ $LARLITE_OS == 'Darwin' ]; then
     export DYLD_LIBRARY_PATH=$SUPERA_LIBDIR:$DYLD_LIBRARY_PATH
 fi
 
