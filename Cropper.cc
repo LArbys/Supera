edab8328aec6708406dec8d6fbf03e2e4c2d2347
Taritree Wongjirad <taritree@mit.edu>
2016-01-06 06:12:35 -0600

    fixed crop extension

--- Cropper.cc
+++ Cropper.cc
@@ -27,8 +27,8 @@ namespace larcaffe {
     }
       
 
-    unsigned int wire_max = 0;
-    unsigned int wire_min = 10000;
+    int wire_max = 0;
+    int wire_min = 10000;
     //const double cm_per_s = 96153.8;
     double earliest_t = 1e10;
     //int zmax = 0;
@@ -47,22 +47,38 @@ namespace larcaffe {
 	//double tick = ts->TPCG4Time2TDC(  );
 	//geo::WireID wireid;
 	geo::WireID wireid = geom->NearestWireID( pos, colplane );  ///const double worldLoc[3], geo::PlaneID const& planeid) const;
-	if ( wireid.Wire<wire_min )
+	if ( (int)wireid.Wire<wire_min )
 	  wire_min = wireid.Wire;
-	if ( wireid.Wire>wire_max )
+	if ( (int)wireid.Wire>wire_max )
 	  wire_max = wireid.Wire;
 	std::cout << "[Cropper] track " << i << " step " << s << ": " << pos[0] << ", " << pos[1] << ", " << pos[2] 
 		  << "t=" << t << "; wire=" << wireid.Wire << " tick=" << ts->TPCG4Time2TDC( t ) << std::endl;
       }
       
     }//end of mctrack loop
-    
+
     std::cout << "[Cropper] Number of mctracks: " << mctracks.size() << std::endl;
     std::cout << "[Cropper] bounding collection wires: [" << wire_min << ", " << wire_max << "]" << std::endl;
     std::cout << "[Cropper] bounding ticks: " << ts->TPCG4Time2TDC( earliest_t ) << " " << ts->TPCG4Time2TDC( earliest_t )+4000 << std::endl;
 
-    if ( wire_max!=0 && wire_min!=10000 ) {
 
+    if ( wire_max!=0 && wire_min!=10000 ) {
+      
+      if ( (wire_max-wire_min)<247 ) {
+	int wire_mean = (int)((wire_max+wire_min)/2);
+	wire_max = wire_mean+124;
+	wire_min = wire_mean-123;
+	std::cout << "[Cropper] extend cropping region: " << wire_min << ", " << wire_max << " mean=" << wire_mean << std::endl;
+	if ( wire_min<0 ) {
+	  wire_max += -wire_min;
+	  wire_min = 0;
+	}
+	else if ( wire_max>=3456 ) {
+	  wire_min -= wire_max-3455;
+	  wire_max = 3455;
+	}
+      }
+      
       MCImage image;
       image.interactionmode = 1;
       image.nu_energy_gev = 1.0;
